e8515867b29f65142171dd10a760a200d7333a49
ludamulcjc025 <ludamulcjc025@student.wethinkcode.co.za>
2026-05-14 11:40:23 +0200

    Initial commit

---
+++ src/test/java/com/platform_domain/fintech_api/FintechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform_domain.fintech_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
