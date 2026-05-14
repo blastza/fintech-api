a8a2e29ff2e9319af86525225136565c89781374
blastza <lutendo.damuleli@outlook.com>
2026-05-14 15:28:57 +0200

    add method to load users from the database during JWT validation

---
+++ src/main/java/com/platform_domain/fintech_api/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.platform_domain.fintech_api.service.impl;
+
+import com.platform_domain.fintech_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .roles("USER")
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+    }
+}
